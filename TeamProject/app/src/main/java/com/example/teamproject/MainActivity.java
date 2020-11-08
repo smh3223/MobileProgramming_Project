c80b4229f4d040a9c81f1a2596fc8e33f43a9b1a
smh3223 <54923850+smh3223@users.noreply.github.com>
2020-11-08 23:10:59 +0900

    BoardTest
    기본 게시판

---
+++ TeamProject/app/src/main/java/com/example/teamproject/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.teamproject;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listview1;
+    ArrayList<Board> board1 = new ArrayList<Board>();
+
+    Button button1;
+    EditText edittext1;
+    EditText edittext2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listview1 = (ListView) findViewById(R.id.listView);
+        final MyAdapter adapter = new MyAdapter(this);
+        listview1.setAdapter(adapter);
+
+        button1 = (Button) findViewById(R.id.button21);
+        edittext1 = (EditText) findViewById(R.id.edittext21);
+        edittext2 = (EditText) findViewById(R.id.edittext22);
+
+
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        database.child("message").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String s) {
+
+                Board board = snapshot.getValue(Board.class);
+                board1.add(board);
+                adapter.Add(board);
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        button1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                click1button(v);
+            }
+        });
+
+
+    }
+
+    public void click1button(View v)
+    {
+        SimpleDateFormat dateform = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        Board board = new Board(edittext1.getText().toString(),dateform.format(date).toString(),edittext2.getText().toString(),1);
+        database.child("message").push().setValue(board);
+        edittext1.setText("");
+        edittext2.setText("");
+
+        Toast.makeText(this, "글을 등록했습니다", Toast.LENGTH_SHORT).show();
+
+    }
+
+}
---
+++ TeamProject/app/src/main/java/com/example/teamproject/MyAdapter.java
@@ -0,0 +1,81 @@
+package com.example.teamproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<Board> data = new ArrayList<Board>();
+
+    MyAdapter(Context context)
+    {
+        this.context = context;
+    }
+
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public int getItemViewType(int position)
+    {
+        return data.get(position).getType();
+    }
+
+    @Override
+    public int getViewTypeCount()
+    {
+        return 2;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        int viewType = getItemViewType(position);
+
+        if(convertView==null)
+        {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_lay, parent, false);
+        }
+
+        TextView textView1 = (TextView) convertView.findViewById(R.id.textView);
+        TextView textView2 = (TextView) convertView.findViewById(R.id.textView2);
+        TextView textView3 = (TextView) convertView.findViewById(R.id.textView3);
+
+        Board board = data.get(position);
+        textView1.setText(board.getTitle());
+        textView2.setText(board.getDate());
+        textView3.setText(board.getContent());
+
+        return convertView;
+
+    }
+
+    public void Add(Board b)
+    {
+        data.add(b);
+    }
+
+
+
+}
